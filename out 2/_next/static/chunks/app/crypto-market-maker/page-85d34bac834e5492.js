b6054c916a316e99f4c4afabd6baad2fad5d4400
Mario <mario@fameuchner.de>
2026-02-05 15:44:30 +0100

    update

--- out 2/_next/static/chunks/app/crypto-market-maker/page-85d34bac834e5492.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405,197,191,493,301,459,883,799,763,868,730,418,999,583,771,357],{4358:function(n,e,u){Promise.resolve().then(u.t.bind(u,231,23))}},function(n){n.O(0,[231,971,23,744],function(){return n(n.s=4358)}),_N_E=n.O()}]);
